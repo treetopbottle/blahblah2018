16a454026067b18c1146df0171769f6005dcd157
treetopbottle <treetopbottle@github>
2018-03-01 19:02:03 +0100

    every car gets one ride

--- solution.py
+++ solution.py
@@ -5,6 +5,11 @@ import sys
 with open(sys.argv[1]) as in_file:
     nr_rows, nr_columns, nr_vehicles, nr_rides, ride_bonus, nr_simulation_steps = [int(i) for i in next(in_file).strip().split(' ')]
 
+    rides = []
     for line in in_file:
-        start_row, start_column, finish_row, finish_column, earliest_start, latest_finish = [int(i) for i in line.strip().split(' ')]
+        a, b, x, y, earliest_start, latest_finish = [int(i) for i in line.strip().split(' ')]
+        ride = [(a,b), (x,y), earliest_start, latest_finish]
+        rides.append(ride)
 
+    for i in range(min(nr_rides, nr_vehicles)):
+        print(1, i)
