b3d7fb21ab5c8047246d35bd07b14289926294f2
treetopbottle <treetopbottle@github>
2018-03-01 18:52:56 +0100

    parsing start

--- solution.py
+++ solution.py
@@ -3,4 +3,6 @@
 import sys
 
 with open(sys.argv[1]) as in_file:
-    pass
+    nr_rows, nr_columns, nr_vehicles, nr_rides, ride_bonus, nr_simulation_steps = [int(i) for i in next(in_file).strip().split(' ')]
+    print(nr_rows, nr_columns, nr_vehicles, nr_rides, ride_bonus, nr_simulation_steps)
+    print(type(nr_rows))
