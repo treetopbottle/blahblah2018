dc7e3a70e991dc522a3f3aa8af1efc0d64f57e6f
treetopbottle <treetopbottle@github>
2018-03-01 18:58:06 +0100

    finish parsing

--- solution.py
+++ solution.py
@@ -4,5 +4,7 @@ import sys
 
 with open(sys.argv[1]) as in_file:
     nr_rows, nr_columns, nr_vehicles, nr_rides, ride_bonus, nr_simulation_steps = [int(i) for i in next(in_file).strip().split(' ')]
-    print(nr_rows, nr_columns, nr_vehicles, nr_rides, ride_bonus, nr_simulation_steps)
-    print(type(nr_rows))
+
+    for line in in_file:
+        start_row, start_column, finish_row, finish_column, earliest_start, latest_finish = [int(i) for i in line.strip().split(' ')]
+
