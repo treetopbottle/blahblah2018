a3b643d42376de5b07350979944d25f812dc7029
treetopbottle <treetopbottle@github>
2018-03-01 22:49:27 +0100

    final try

--- solution.py
+++ solution.py
@@ -18,7 +18,7 @@ def find_best_ride(rides, rides_left, position, time, bonus):
             wait_to_start = earliest_start-time
             wait_time = max(to_ride_dist, wait_to_start)
             if wait_to_start >= 0: # bonus!!
-                bonus = 1 + max(0.0001, float(bonus) / 11)
+                bonus = 1 + max(0, float(bonus) / 25)
                 wait_time = float(wait_time) / bonus
             if wait_time < min_wait_time:
                 best_pos = (x, y)
