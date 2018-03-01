4ed5798e9bdefb9776f4d6a235c78880a05355d8
treetopbottle <treetopbottle@github>
2018-03-01 21:46:10 +0100

    push voor mark

--- parallel.sh
+++ parallel.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-./solution.py input/a_example.in        > output/a_example.in; &
-./solution.py input/b_should_be_easy.in > output/b_should_be_easy.in; &
-./solution.py input/c_no_hurry.in       > output/c_no_hurry.in; &
-./solution.py input/d_metropolis.in     > output/d_metropolis.in; &
-./solution.py input/e_high_bonus.in     > output/e_high_bonus.in; &
+./solution.py input/a_example.in        > output/a_example.in &
+./solution.py input/b_should_be_easy.in > output/b_should_be_easy.in &
+./solution.py input/c_no_hurry.in       > output/c_no_hurry.in &
+./solution.py input/d_metropolis.in     > output/d_metropolis.in &
+./solution.py input/e_high_bonus.in     > output/e_high_bonus.in &
+
+wait
 
--- solution.py
+++ solution.py
@@ -4,22 +4,33 @@ import sys
 import collections
 
 
-def find_best_ride(rides, rides_left, position, time):
+def find_best_ride(rides, rides_left, position, time, bonus):
+    min_wait_time = 9999999
+    best_ride = -1
+    best_pos = (0,0)
+    best_time = 0
     for ride in rides_left:
         (a, b), (x, y), earliest_start, latest_finish = rides[ride]
         to_ride_dist = abs(position[0]-a) + abs(position[1]-b)
         ride_dist = abs(a-x) + abs(b-y)
         possible = latest_finish > time + to_ride_dist + ride_dist
         if possible:
-            rides_left.remove(ride)
-            new_pos = (x, y)
-            new_time = ride_dist + max(earliest_start, time+to_ride_dist)
-            return ride, new_pos, new_time
-    return -1, (0,0), 0
+            wait_to_start = earliest_start-time
+            wait_time = max(to_ride_dist, wait_to_start)
+            if wait_to_start >= 0: # bonus!!
+                wait_time -= bonus
+            if wait_time < min_wait_time:
+                best_pos = (x, y)
+                best_time = ride_dist + max(earliest_start, time+to_ride_dist)
+                best_ride = ride
+                min_wait_time = wait_time
+    if best_ride >= 0:
+        rides_left.remove(best_ride)
+    return best_ride, best_pos, best_time
 
 
 with open(sys.argv[1]) as in_file:
-    nr_rows, nr_columns, nr_vehicles, nr_rides, ride_bonus, nr_simulation_steps = [int(i) for i in next(in_file).strip().split(' ')]
+    nr_rows, nr_columns, nr_vehicles, nr_rides, bonus, nr_simulation_steps = [int(i) for i in next(in_file).strip().split(' ')]
 
     rides = []
     rides_left = set()
@@ -35,7 +46,7 @@ with open(sys.argv[1]) as in_file:
         time = 0
         while True:
             positions_for_vehicle = []
-            best_ride_nr, new_pos, new_time = find_best_ride(rides, rides_left, pos, time)
+            best_ride_nr, new_pos, new_time = find_best_ride(rides, rides_left, pos, time, bonus)
             if best_ride_nr < 0:
                 break
             vehicle_rides[veh_nr].append(best_ride_nr)
